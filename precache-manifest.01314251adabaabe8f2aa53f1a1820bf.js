282bcd206b93f20e3e8118548fbed031abe67d03
KavindhaDissanayaka <92946@fhss.sjp.ac.lk>
2021-06-11 19:46:23 -0700

    Updates

---
+++ precache-manifest.01314251adabaabe8f2aa53f1a1820bf.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f641b206bf84734ac29fefd15e0b0bbc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f3b2aa824b906c0ed7ad",
+    "url": "/static/css/main.956fad23.chunk.css"
+  },
+  {
+    "revision": "94833f0d9aaa3be6210f",
+    "url": "/static/js/2.19e99e5d.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/js/2.19e99e5d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f3b2aa824b906c0ed7ad",
+    "url": "/static/js/main.6ed27f6e.chunk.js"
+  },
+  {
+    "revision": "f52ba179432c3be28857",
+    "url": "/static/js/runtime-main.554504a0.js"
+  },
+  {
+    "revision": "36cf85b5f2913478492242387807d01d",
+    "url": "/static/media/ksd.36cf85b5.jpeg"
+  },
+  {
+    "revision": "ff5efd0d8eb4e221cce3772ad707dcde",
+    "url": "/static/media/resume.ff5efd0d.pdf"
+  }
+]);
